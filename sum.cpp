e67c7eb6ebbe88d75fa2a44af40500c9112deac4
ektaggu <ektasharmabsp@gmail.com>
2022-01-07 20:13:50 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int first_number = 0, second_number = 0;
+	float result = 0;
+	
+	cout << "enter your first_number: ";
+	cin >> first_number;
+	
+	cout << "enter your second_number: ";
+	cin >> second_number;
+	
+	result = first_number + second_number;
+
+	cout << "sum of numbers: " << result ;
+
+	
+	return 0;
+}
